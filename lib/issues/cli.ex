3e2226a91bca1924703df24ce444a9e6b227d2a7
Michael Stock <mikeastock@gmail.com>
2015-06-20 17:31:54 -0700

    TableFormatter

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,5 +1,7 @@
 defmodule Issues.CLI do
 
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
@@ -43,11 +45,13 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_hashdicts
     |> sort_by_created_at(:asc)
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
